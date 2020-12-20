30e297f49c5c15f050a51fc78ff6f4a8ad0f88fc
서재원 <cinos81@gmail.com>
2020-12-20 15:38:20 +0900

    Add Dynamic Route

--- README.md
+++ README.md
@@ -1,17 +1,19 @@
-# cra-template-typescript-eslint-prettier
+# cra-template-dynamic-route
 
-This project is forked from the official Typescript template for [Create React App](https://github.com/facebook/create-react-app).
+This project is forked from the Typescript template for [Create React App](https://github.com/simonepizzamiglio/cra-template-typescript-eslint-prettier).
 
 It comes with eslint and prettier, rules for them can be edited in `.eslintrc` file.
 
-To use this template, add `--template typescript-eslint-prettier` when creating a new app.
+To use this template, add `--template dynamic-route` when creating a new app.
+
+inspired by [min9nim](https://min9nim.now.sh/2020-12-18-react-routing/?fbclid=IwAR1DJWH6Kk8DMTqSk2a1EE2k8n4bQifrJwMwDN6tVfFlA2YKCDzIsohlQ4s)
 
 For example:
 
 ```sh
-npx create-react-app my-app --template typescript-eslint-prettier
+npx create-react-app my-app --template dynamic-route
 
 # or
 
-yarn create react-app my-app --template typescript-eslint-prettier
+yarn create react-app my-app --template dynamic-route
 ```
--- template/src/App.tsx
+++ template/src/App.tsx
@@ -1,23 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
+import { DynamicRoutes } from './DynamicRoutes';
+import { BrowserRouter, Link } from 'react-router-dom';
 import './App.css';
+
+const Loading = () => <div>Loading...</div>
 
 function App() {
 	return (
 		<div className="App">
 			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.tsx</code> and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
+				<BrowserRouter>
+					<ul>
+						<li>
+							<Link to="/">To Main Page</Link>
+						</li>
+						<li>
+							<Link to="/sub">To Sub Page</Link>
+						</li>
+					</ul>
+					<DynamicRoutes Loading={<Loading />} />
+					<p>
+						Add some pages under<code>src/pages</code> and save to reload.
+					</p>
+					<a
+						className="App-link"
+						href="https://reactjs.org"
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						Learn React
+					</a>
+				</BrowserRouter>
 			</header>
 		</div>
 	);
